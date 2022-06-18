23725fc10bc649c5e77c4d738f59e056cf021b07
Murray Kornelsen <murray.kornelsen@mail.mcgill.ca>
2022-06-18 18:51:39 -0400

    remove

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode'
 
+import * as http2 from 'http2';
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('ARM64 extension activated');
 
@@ -8,6 +10,8 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(helloWorldCommand);
+
+    const instructions = fetchInstructionList();
 
     vscode.languages.registerHoverProvider('arm64', {
         provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
@@ -19,4 +23,34 @@ export function activate(context: vscode.ExtensionContext) {
     });
 }
 
+function fetchInstructionList() {
+    const client = http2.connect('https://documentation-service.arm.com');
 
+    client.on('error', (err) => console.error(err));
+    const req = client.request({
+        ':path': '/documentation/ddi0602/2022-03/Base-Instructions'
+    });
+
+    req.on('response', (headers, flags) => {
+        for (const name in headers) {
+            console.log(`${name}: ${headers[name]}`);
+        }
+    });
+    
+    req.setEncoding('utf8');
+    let data = '';
+    req.on('data', (chunk) => { data += chunk; });
+    req.on('end', () => {
+        client.close();
+        
+        console.log(`\n${data}`);
+
+        let parsed = JSON.parse(data).content;
+        console.log(parsed);
+        let buff = Buffer.from(parsed, 'base64');
+        let text = buff.toString('ascii');
+        console.log(text);
+    });
+    req.end();
+
+}
