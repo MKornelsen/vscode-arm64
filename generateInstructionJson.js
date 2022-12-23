875e3654cf38f3a679ed6296da8d5c98718e7b11
Murray Kornelsen <murray.kornelsen@mail.mcgill.ca>
2022-12-22 23:40:56 -0500

    run

--- generateInstructionJson.js
+++ generateInstructionJson.js
@@ -43,41 +43,56 @@ const testPromise = new Promise(resolve => {
             let parsed = JSON.parse(fulldata);
             let decoded = Buffer.from(parsed.content, 'base64').toString('ascii');
             fs.writeFileSync('exampledetail.html', decoded);
+            resolve();
         });
     });
-
 });
+
+testPromise.then(() => {
+    console.log('testPromise resolved');
+});
+
+let instructions = [];
 
 const promises = [];
 
-let instructiondetail = [];
+const idstatus = new Map();
 
-let instructions = [];
+const loadDetail = (line) => new Promise((resolve, reject) => {
+    const match = line.match(regexp);
+    https.get('https://documentation-service.arm.com' + match[1], res => {
+        console.log(match[2] + ': ' + res.statusCode);
+        idstatus.set(match[2], 'open');
+        if (res.statusCode != 200) {
+            reject('uh oh bad exit code');
+        }
+        // console.log(res.headers);
+
+        let fulldata = '';
+        res.on('data', data => {
+            // console.log(data.toString());
+            fulldata = fulldata + data.toString();
+        });
+
+        res.on('end', () => {
+            let parsed = JSON.parse(fulldata);
+            let decoded = Buffer.from(parsed.content, 'base64').toString('ascii');
+            // console.log(decoded);
+            const result = {
+                instruction: match[2],
+                encdetail: parsed.content
+            };
+            // console.log(match[2] + ' done');
+            idstatus.set(match[2], 'done');
+            resolve(result);
+        });
+    }).on('error', () => {
+        console.log(match[2] + ' had an error');
+    });
+});
+
 lines.forEach(line => {
     let match = line.match(regexp);
-    promises.push(new Promise(resolve => {
-            https.get('https://documentation-service.arm.com' + match[1], res => {
-            console.log(match[2] + ': ' + res.statusCode);
-            // console.log(res.headers);
-
-            let fulldata = '';
-            res.on('data', data => {
-                // console.log(data.toString());
-                fulldata = fulldata + data.toString();
-            });
-
-            res.on('end', () => {
-                let parsed = JSON.parse(fulldata);
-                let decoded = Buffer.from(parsed.content, 'base64').toString('ascii');
-                // console.log(decoded);
-                instructiondetail.push({
-                    instruction: match[2],
-                    encdetail: parsed.content
-                });
-                resolve();
-            });
-        });
-    }));
 
     let inst = match[2].replaceAll('&amp;', '&').replaceAll('&lt;', '<').replaceAll('&gt;', '>');
     instructions.push({
@@ -88,10 +103,32 @@ lines.forEach(line => {
     });
 });
 
+fs.writeFileSync('InstructionData.json', JSON.stringify(instructions, null, '    '));
 
-Promise.all(promises).then(() => {
-    
-    // fs.writeFileSync('InstructionDetail.json', JSON.stringify(instructiondetail, null, '    '))
-});
+async function loadAllDetails() {
+    const instructiondetail = [];
+    for (const line of lines) {
+        const idetail = await loadDetail(line);
+        instructiondetail.push(idetail);
+    }
+    fs.writeFileSync('InstructionDetail.json', JSON.stringify(instructiondetail, null, '    ')); 
+}
+loadAllDetails();
 
-fs.writeFileSync('out/InstructionData.json', JSON.stringify(instructions, null, '    '));
+setTimeout(() => {
+    // console.log('doing idstatus check');
+    // console.log(idstatus);
+    console.log(idstatus.size);
+    let donecount = 0;
+    let othercount = 0;
+    idstatus.forEach((value, key) => {
+        if (value != 'done') {
+            console.log(`${key}: ${idstatus.get(key)}`);
+            donecount++;
+        } else {
+            othercount++;
+        }
+    });
+    console.log(`done: ${donecount}\nother: ${othercount}`);
+    console.log(lines.length);
+}, 30000)
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 
 import * as fs from 'fs';
 import * as path from 'path';
+import { exec } from 'child_process';
 
 interface InstructionData {
     instruction: string;
@@ -30,38 +31,63 @@ const conditioncodes = [
     'al'
 ];
 
-function fetchInstructionList(): InstructionData[] {
-    let data = fs.readFileSync(path.resolve(__dirname, 'InstructionData.json'));
-    return JSON.parse(data.toString());
+const instructionMap = new Map<string, InstructionData[]>();
+
+function fetchInstructionList(filepath: string) {
+    let data = fs.readFileSync(filepath);
+    const instructions: InstructionData[] = JSON.parse(data.toString());
+
+    instructions.forEach(inst => {
+        let key = inst.key.toLowerCase();
+        if (!instructionMap.has(key)) {
+            instructionMap.set(key, []);
+        }
+        instructionMap.get(key)?.push(inst);
+    });
+
+    conditioncodes.forEach(cc => {
+        // console.log(`b${cc}`);
+        const bcond = instructionMap.get('b.cond');
+        const bccond = instructionMap.get('bc.cond');
+        if (bcond === undefined || bccond === undefined) {
+            console.log('b or bc instruction not found')
+            return;
+        }
+        instructionMap.set('b' + cc, bcond);
+        instructionMap.set('bc' + cc, bccond);
+    });
 }
 
-const instructions = fetchInstructionList();
+const mkexec = (cmd: string, cwd: string) => {
+    return new Promise<string[]>((resolve) => {
+        exec(cmd, {
+            cwd: cwd
+        }, (error, stdout, stderr) => {
+            resolve([stdout, stderr]);
+        });
+    });
+}
 
-const instructionMap = new Map<string, InstructionData[]>();
-instructions.forEach(inst => {
-    let key = inst.key.toLowerCase();
-    if (!instructionMap.has(key)) {
-        instructionMap.set(key, []);
+async function execsequence(cmds: string[], cwd: string, outputstream: vscode.OutputChannel) {
+    outputstream.show();
+    for (const cmd of cmds) {
+        outputstream.appendLine('$ ' + cmd);
+        const [stdout, stderr] = await mkexec(cmd, cwd);
+        if (stdout != '') {
+            outputstream.appendLine(stdout);
+        }
+        if (stderr != '') {
+            outputstream.appendLine(stderr);
+        }
     }
-    instructionMap.get(key)?.push(inst);
-});
-
-conditioncodes.forEach(cc => {
-    // console.log(`b${cc}`);
-    const bcond = instructionMap.get('b.cond');
-    const bccond = instructionMap.get('bc.cond');
-    if (bcond === undefined || bccond === undefined) {
-        console.log('b or bc instruction not found')
-        return;
-    }
-    instructionMap.set('b' + cc, bcond);
-    instructionMap.set('bc' + cc, bccond);
-});
-
-const instructionCompletions = new vscode.CompletionList();
+}
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('ARM64 extension activated');
+    console.log(context.asAbsolutePath('InstructionData.json'));
+    fetchInstructionList(context.asAbsolutePath('InstructionData.json'));
+
+    let outputstream = vscode.window.createOutputChannel('vscode-arm64', 'ARM64');
 
     const helloWorldCommand = vscode.commands.registerCommand('arm64.helloWorld', () => {
         vscode.window.showInputBox({
@@ -78,30 +104,88 @@ export function activate(context: vscode.ExtensionContext) {
         });
         vscode.window.showInformationMessage('Hello ARM64');
     });
-
+    
     const androidRunCommmand = vscode.commands.registerCommand('arm64.androidrun', () => {
         const conf = vscode.workspace.getConfiguration('vscode-arm64');
         const adbpath = conf.adbpath;
         vscode.window.showInformationMessage(`Using adb: ${adbpath}`);
-
+        if (adbpath == '') {
+            vscode.window.showWarningMessage('You should probably set the adb executable in preferences');
+        }
+    
         vscode.window.showInputBox({
-            prompt: "Executable name"
+            prompt: "Executable name (leave blank for a.out)"
         }).then((name) => {
-            if (name === undefined) {
+            if (name === undefined || name === '') {
+                name = 'a.out';
+            }
+    
+            const wfs = vscode.workspace.workspaceFolders;
+            if (wfs == undefined || wfs.length < 1) {
+                vscode.window.showErrorMessage('You need to have a workspace open');
                 return;
             }
-            let term = vscode.window.createTerminal("Android Run");
-            term.show();
-            term.sendText('dir');
+            const cwd = wfs[0].uri.fsPath;
+            console.log(cwd);
+    
+            const pushcmd = `${adbpath} push ${name} /data/local/tmp`;
+            const chmodcmd = `${adbpath} shell chmod +x /data/local/tmp/${name}`;
+            const runcmd = `${adbpath} shell /data/local/tmp/${name}`;
+    
+            execsequence([pushcmd, chmodcmd, runcmd], cwd, outputstream);
         });
-        // vscode.window.showQuickPick()
     });
-
+    
     const clangCompileCommand = vscode.commands.registerCommand('arm64.clangcompile', () => {
         const conf = vscode.workspace.getConfiguration('vscode-arm64');
         const clangpath = conf.clangpath;
         const linkerpath = conf.linkerpath;
         vscode.window.showInformationMessage(`Using clang: ${clangpath}, linker: ${linkerpath}`);
+        if (clangpath == '' || linkerpath == '') {
+            vscode.window.showWarningMessage(`Make sure your clangpath and linkerpath are set`);
+        }
+        
+        const wfs = vscode.workspace.workspaceFolders;
+        if (wfs == undefined || wfs.length < 1) {
+            vscode.window.showErrorMessage('You need to have a workspace open');
+            return;
+        }
+        // console.log(wfs[0]);
+        const cwd = wfs[0].uri.fsPath;
+        
+        const pattern = new vscode.RelativePattern(wfs[0], '**/*.{s,S,asm}');
+        vscode.workspace.findFiles(pattern).then((filepaths) => {
+            console.log(filepaths);
+            const filelist = filepaths.map((uri) => {
+                return path.relative(wfs[0].uri.toString(), uri.toString());
+            });
+            vscode.window.showQuickPick(filelist, {
+                canPickMany: true,
+                title: 'Files to assemble'
+            }).then((files) => {
+                console.log(files);
+                if (files === undefined || files.length < 1) {
+                    vscode.window.showErrorMessage('Select at least one file to assemble');
+                    return;
+                }
+    
+                const cmdseq = [];
+    
+                files.forEach((fn) => {
+                    const command = `${clangpath} -x assembler -target aarch64-linux-android -c ${fn} -o ${fn}.o`;
+                    cmdseq.push(command);
+                });
+    
+                let linkercmd = `${linkerpath}`;
+                files.forEach((fn) => {
+                    linkercmd = `${linkercmd} ${fn}.o`;
+                });
+                linkercmd = `${linkercmd} -o a.out`
+                cmdseq.push(linkercmd);
+    
+                execsequence(cmdseq, cwd, outputstream);
+            });
+        });
     });
 
     context.subscriptions.push(helloWorldCommand);
@@ -142,6 +226,52 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    const buildLabelList = () => {
+        
+        const wfs = vscode.workspace.workspaceFolders;
+        if (wfs == undefined || wfs.length < 1) {
+            return;
+        }
+        // console.log(wfs[0]);
+        
+        const labelregex = new RegExp('\\s*(\\w+):');
+        const labels: string[] = []
+
+        const pattern = new vscode.RelativePattern(wfs[0], '**/*.{s,S,asm}');
+        vscode.workspace.findFiles(pattern).then((filepaths) => {
+            filepaths.forEach((uri) => {
+                vscode.workspace.openTextDocument(uri).then((doc) => {
+                    // console.log(doc.fileName);
+                    for (let i = 0; i < doc.lineCount; i++) {
+                        const line = doc.lineAt(i).text;
+                        const labelmatch = line.match(labelregex);
+                        if (labelmatch !== null) {
+                            console.log(labelmatch[1]);
+                            labels.push(labelmatch[1]);
+                        }
+                    }
+                });
+            }); 
+        });
+        return labels;
+    }
+
+    const labels = buildLabelList();
+
+    vscode.workspace.onDidChangeTextDocument((docChangeEvent) => {
+        console.log(docChangeEvent.document.fileName);
+    })
+
+    vscode.languages.registerCompletionItemProvider('arm64', {
+        provideCompletionItems: function (document, position, token, context) {
+            console.log('Completion invoked');
+            // console.log(`completion at: ${document.fileName}, position: ${position}`);
+            
+            const doctext = document.getText();
+            // console.log(doctext);
+            return null;
+        }
+    });
     /*
     vscode.languages.registerCompletionItemProvider('arm64', {
         provideCompletionItems(document, position, token, context) {
@@ -164,3 +294,5 @@ export function activate(context: vscode.ExtensionContext) {
     });
     */
 }
+
+export function deactivate() {}
